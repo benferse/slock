1f91fb8a0f4cde351a81cbc192a57907c10be31b
Ben Fersenheim <ben@bu.edu>
2022-04-15 21:50:49 -0400

    slock updates

--- config.mk
+++ config.mk
@@ -4,7 +4,7 @@ VERSION = 1.4
 # Customize below to fit your system
 
 # paths
-PREFIX = /usr/local
+PREFIX = ~/.local
 MANPREFIX = ${PREFIX}/share/man
 
 X11INC = /usr/X11R6/include
@@ -12,7 +12,7 @@ X11LIB = /usr/X11R6/lib
 
 # includes and libs
 INCS = -I. -I/usr/include -I${X11INC}
-LIBS = -L/usr/lib -lc -lcrypt -L${X11LIB} -lX11 -lXext -lXrandr
+LIBS = -L/usr/lib -lc -lcrypt -L${X11LIB} -lX11 -lXext -lXrandr -lXinerama
 
 # flags
 CPPFLAGS = -DVERSION=\"${VERSION}\" -D_DEFAULT_SOURCE -DHAVE_SHADOW_H
